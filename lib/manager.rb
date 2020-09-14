5b1a9ab9f18e350c8a9c82bf97f17a93fdb27200
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-14 17:40:12 -0600

    Add basic initialize and framework for class

--- lib/manager.rb
+++ lib/manager.rb
@@ -1,4 +1,17 @@
 require 'csv'
 
 class Manager
+  attr_reader :manager
+
+  def initialize(csv_file_path, manager_type)
+    @manager = create_manager(csv_file_path, manager_type)
+  end
+
+  def from_csv(csv_file_path)
+    CSV.foreach(csv_file_path, headers: true,)
+  end
+
+  def create_manager
+
+  end
 end
