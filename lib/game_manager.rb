bdf006be234059bb3975f88bbef703376320b6a8
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-14 20:06:57 -0500

    Change if statment to next guard clause in GameManager class

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -36,12 +36,12 @@ class GameManager
   def team_stats
     tracker.initialize_team_stats_hash.each do |team_id, games_goals|
       games.each do |game|
-        if team_id == game.away_team_id || team_id == game.home_team_id
-          games_goals[:away_games] += 1 if team_id == game.away_team_id
-          games_goals[:home_games] += 1 if team_id == game.home_team_id
-          games_goals[:away_goals] += game.away_goals.to_i if team_id == game.away_team_id
-          games_goals[:home_goals] += game.home_goals.to_i if team_id == game.home_team_id
-        end
+        next unless team_id == game.away_team_id || team_id == game.home_team_id
+        
+        games_goals[:away_games] += 1 if team_id == game.away_team_id
+        games_goals[:home_games] += 1 if team_id == game.home_team_id
+        games_goals[:away_goals] += game.away_goals.to_i if team_id == game.away_team_id
+        games_goals[:home_goals] += game.home_goals.to_i if team_id == game.home_team_id
       end
       games_goals[:total_games] = games_goals[:away_games] + games_goals[:home_games]
       games_goals[:total_goals] = games_goals[:away_goals] + games_goals[:home_goals]
